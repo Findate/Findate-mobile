054f4f23c2596f72a94402950c448cadd5183f3c
kolikay <kolikay1989@gmail.com>
2022-09-22 11:17:56 +0100

    kolikay first commit

---
+++ lib/widgets/reusesable_widget/normal_text.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+
+
+class NormalText extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double? size = 14;
+  TextOverflow overflow;
+  FontWeight? fontWeight;
+  TextAlign? textAlign;
+
+  NormalText(
+      {Key? key,
+      this.color = Colors.black,
+      required this.text,
+      this.overflow = TextOverflow.ellipsis,
+      this.size,
+      this.fontWeight = FontWeight.normal,
+      this.textAlign,
+      
+      })
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+    
+    
+    );
+  }
+}
