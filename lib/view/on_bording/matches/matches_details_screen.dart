15eb8595cf94dff614a8abd1d6387972d97e7437
kolikay <kolikay1989@email.com>
2022-10-11 14:17:52 +0100

    fixed render bugs in match infomscreen

--- lib/view/on_bording/matches/matches_details_screen.dart
+++ lib/view/on_bording/matches/matches_details_screen.dart
@@ -17,51 +17,55 @@ class _MatchUserInfoState extends State<MatchUserInfo> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: SizedBox(
-        height: 800.h,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ReusesableAppbarButton(
-                  iconButton: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.keyboard_arrow_left_outlined,
-                      color: AppColor.mainColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: SizedBox(
+          height: 900.h,
+          width: 343.w,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ReusesableAppbarButton(
+                    iconButton: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.keyboard_arrow_left_outlined,
+                        color: AppColor.mainColor,
+                      ),
                     ),
                   ),
-                ),
-                NormalText(
-                  text: 'Popular Matches',
-                  size: 22.sp,
-                  fontWeight: FontWeight.w600,
-                ),
-                ReusesableAppbarButton(
-                  iconButton: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.tune_outlined,
-                      color: AppColor.mainColor,
+                  NormalText(
+                    text: ' Popular Matches ',
+                    size: 22.sp,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  ReusesableAppbarButton(
+                    iconButton: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.tune_outlined,
+                        color: AppColor.mainColor,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            // MatchCard()
-            SizedBox(
-              height: 715.h,
-              width: 320.w,
-              child: ListView.builder(
-                  itemCount: 1,
-                  itemBuilder: (context, int i) {
-                    return const MatchCard();
-                  }),
-            ),
-          ],
+                ],
+              ),
+              // MatchCard()
+              SizedBox(
+                height: 715.h,
+                width: 343.w,
+                child: ListView.builder(
+                    itemCount: 2,
+                    itemBuilder: (context, int i) {
+                      return const MatchCard();
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
     ));
@@ -79,138 +83,155 @@ class MatchCard extends StatefulWidget {
 class _MatchCardState extends State<MatchCard> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          children: [
-            Container(color: Colors.white, height: 200,),
-            Container(
-              color: Colors.pink[50],
-              height: 305.h,
-              width: 320.w,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    NormalText(
-                      text: 'Tiana Joel',
-                      size: 16.sp,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    Row(
-                      children: [
-                        const Icon(
-                          // find right icon
-                          Icons.do_not_disturb_on_total_silence_rounded,
-                          color: Colors.green,
-                          size: 18,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 5.0),
-                          child: NormalText(
-                            text: 'Recently Active',
-                            size: 12.sp,
-                            color: AppColor.grey400,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        const Icon(
-                          Icons.location_on,
-                          color: AppColor.mainColor,
-                          size: 18,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 5.0),
-                          child: NormalText(
-                            text: 'Lagos',
-                            size: 12.sp,
-                            color: AppColor.grey400,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(children: [
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              //container behind image
+              Container(
+                color: Colors.white,
+                height: 300,
+              ),
+              //container which image is slightly on
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.r),
+                  gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.topRight,
+                      colors: [Color(0xffffccd6), Color(0xffcfccff)]),
+                ),
+                height: 305.h,
+                width: 343.w,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //sized box to push info content downwards
+                      SizedBox(
+                        height: 120.h,
+                      ),
                       NormalText(
-                          text:
-                              'Lorem ipsum dolor sit amet, \n consectetur adpiscing elit.',
-                          size: 12.sp,
-                          color: AppColor.grey400),
-                    ]),
-                    SizedBox(
-                      height: 40.w,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        SizedBox(
-                          width: 40.w,
-                        ),
-                        Container(
-                          height: 40.h,
-                          width: 40.w,
-                          decoration: const BoxDecoration(
-                            color: AppColor.secondaryMain,
-                            shape: BoxShape.circle,
+                        text: 'Tiana Joel',
+                        size: 16.sp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            // find right icon
+                            Icons.do_not_disturb_on_total_silence_rounded,
+                            color: Colors.green,
+                            size: 18,
                           ),
-                          child: const Icon(Icons.close, color: Colors.white),
-                        ),
-                        SizedBox(
-                          width: 6.w,
-                        ),
-                        Container(
-                          height: 40.h,
-                          width: 40.w,
-                          decoration: const BoxDecoration(
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5.0),
+                            child: NormalText(
+                              text: 'Recently Active',
+                              size: 12.sp,
+                              color: AppColor.grey400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on,
                             color: AppColor.mainColor,
-                            shape: BoxShape.circle,
+                            size: 18,
                           ),
-                          child: const Icon(Icons.favorite, color: Colors.white),
-                        ),
-                        SizedBox(
-                          width: 6.w,
-                        ),
-                        Container(
-                          height: 40.h,
-                          width: 40.w,
-                          decoration: const BoxDecoration(
-                            color: Colors.deepPurpleAccent,
-                            shape: BoxShape.circle,
+                          Padding(
+                            padding: const EdgeInsets.only(left: 5.0),
+                            child: NormalText(
+                              text: 'Lagos',
+                              size: 12.sp,
+                              color: AppColor.grey400,
+                            ),
                           ),
-                          child: const Icon(Icons.message_rounded,
-                              color: Colors.white),
-                        ),
-                        SizedBox(
-                          width: 40.w,
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                      Row(children: [
+                        NormalText(
+                            text:
+                                'Lorem ipsum dolor sit amet,\n consectetur adpiscing elit.',
+                            size: 12.sp,
+                            color: Colors.black),
+                      ]),
+                      SizedBox(
+                        height: 40.w,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SizedBox(
+                            width: 40.w,
+                          ),
+                          Container(
+                            height: 40.h,
+                            width: 40.w,
+                            decoration: const BoxDecoration(
+                              color: AppColor.secondaryMain,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(Icons.close, color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 6.w,
+                          ),
+                          Container(
+                            height: 40.h,
+                            width: 40.w,
+                            decoration: const BoxDecoration(
+                              color: AppColor.mainColor,
+                              shape: BoxShape.circle,
+                            ),
+                            child:
+                                const Icon(Icons.favorite, color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 6.w,
+                          ),
+                          Container(
+                            height: 40.h,
+                            width: 40.w,
+                            decoration: const BoxDecoration(
+                              color: Colors.deepPurpleAccent,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(Icons.message_rounded,
+                                color: Colors.white),
+                          ),
+                          SizedBox(
+                            width: 40.w,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-        Positioned(
-          top: -10,
-          right: 5,
-          left: 5,
-          child: Container(
-            height: 316.h,
-            width: 310.w,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(20.r),
-              image: const DecorationImage(
-                  image: AssetImage('assets/homeImage3.png'),
-                  fit: BoxFit.cover),
+            ],
+          ),
+          Positioned(
+            top: 0,
+            right: 5,
+            left: 5,
+            child: Container(
+              height: 400.h,
+              // width: 300.w,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.r),
+                image: const DecorationImage(
+                    image: AssetImage('assets/homeImage3.png'), fit: BoxFit.fill),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
